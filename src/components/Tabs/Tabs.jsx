8dde71e40498e5bce9861690391e59aff18c5619
Andrii Chornyi <ixxxx@i.ua>
2024-12-02 14:23:33 +0200

    your commit message

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,25 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+
+  return (
+    <ul>
+      {tabs.map(tab => (
+        <li
+          data-cy="Tab"
+          key={tab.id}
+          onClick={() => {
+            if (tab.id !== activeTabId) {
+              onTabSelected(tab.id);
+            }
+          }}
+          className={cn({ "is-active": tab.id === activeTabId})}
+        >
+          <a href={`#${tab.id}`} data-cy="TabLink" >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  )
+};
